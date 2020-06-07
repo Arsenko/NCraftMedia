adedf2ea0192fa4072b81fd08d790fe17bf96bbd
Arsenko <minsenka@gmail.com>
2020-06-07 15:41:42 +0300

    finished

--- app/src/main/java/ru/minnullin/PostViewHolder.kt
+++ app/src/main/java/ru/minnullin/PostViewHolder.kt
@@ -8,12 +8,16 @@ import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
+import com.google.gson.JsonElement
 import com.minnullin.models.CounterType
 import io.ktor.client.HttpClient
 import io.ktor.client.features.json.GsonSerializer
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.request.post
+import io.ktor.client.request.url
+import io.ktor.content.TextContent
 import io.ktor.http.ContentType
+import io.ktor.http.HttpMethod
 import io.ktor.util.KtorExperimentalAPI
 import kotlinx.android.synthetic.main.item_post.view.*
 import kotlinx.coroutines.CoroutineScope
@@ -50,7 +54,12 @@ class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
                 }
                 PostType.Video -> {
-                    postImage.setImageDrawable(post.postImage.let{getDrawableFromAbstractInt(context,0)})
+                    postImage.setImageDrawable(post.postImage.let {
+                        getDrawableFromAbstractInt(
+                            context,
+                            0
+                        )
+                    })
                     postImage.setOnClickListener {
                         val watchVideo = Intent().apply {
                             action = Intent.ACTION_VIEW
@@ -63,7 +72,12 @@ class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
                 }
                 PostType.Advertising -> {
-                    postImage.setImageDrawable(post.postImage?.let { getDrawableFromAbstractInt(context,it) })
+                    postImage.setImageDrawable(post.postImage?.let {
+                        getDrawableFromAbstractInt(
+                            context,
+                            it
+                        )
+                    })
                     postImage.setOnClickListener {
                         val watchVideo = Intent().apply {
                             action = Intent.ACTION_VIEW
@@ -91,15 +105,15 @@ class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
             } else {
                 likeButton.setImageDrawable(getDrawable(context, R.drawable.ic_favorite_grey_24dp))
             }
-            dislikeNumber.text=if(post.dislikeCounter<=0){
+            dislikeNumber.text = if (post.dislikeCounter <= 0) {
                 ""
-            }else{
+            } else {
                 post.dislikeCounter.toString()
             }
-            if(post.dislikedByMe){
-                dislikeButton.setImageDrawable(getDrawable(context,R.drawable.ic_dislike_red))
-            }else{
-                dislikeButton.setImageDrawable(getDrawable(context,R.drawable.ic_dislike_grey))
+            if (post.dislikedByMe) {
+                dislikeButton.setImageDrawable(getDrawable(context, R.drawable.ic_dislike_red))
+            } else {
+                dislikeButton.setImageDrawable(getDrawable(context, R.drawable.ic_dislike_grey))
             }
             likeButton.setOnClickListener {
                 post.likedByMe = if (!post.likedByMe) {
@@ -110,6 +124,9 @@ class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
                         )
                     )
                     post.likeIncrease()
+                    CoroutineScope(IO).launch {
+                        changeCounter(post, CounterType.Like)
+                    }
                     adapter.notifyItemChanged(position)
                     true
                 } else {
@@ -120,6 +137,9 @@ class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
                         )
                     )
                     post.likeDecrease()
+                    CoroutineScope(IO).launch {
+                        changeCounter(post, CounterType.Like)
+                    }
                     adapter.notifyItemChanged(position)
                     false
                 }
@@ -134,7 +154,7 @@ class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
                     )
                     post.dislikeIncrease()
                     CoroutineScope(IO).launch {
-                        changeCounter(post,CounterType.Dislike)
+                        changeCounter(post, CounterType.Dislike)
                     }
                     adapter.notifyItemChanged(position)
                     true
@@ -147,7 +167,7 @@ class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
                     )
                     post.dislikeDecrease()
                     CoroutineScope(IO).launch {
-                        changeCounter(post,CounterType.Dislike)
+                        changeCounter(post, CounterType.Dislike)
                     }
                     adapter.notifyItemChanged(position)
                     false
@@ -165,39 +185,60 @@ class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
                 post.shareIncrease()
                 context.startActivity(intent)
                 CoroutineScope(IO).launch {
-                    changeCounter(post,CounterType.Share)
+                    changeCounter(post, CounterType.Share)
                 }
             }
         }
     }
-    private fun getDrawableFromAbstractInt(context:Context, value:Int) =
-        when(value){
-            0-> getDrawable(context,R.mipmap.ad_foreground)
-            1-> getDrawable(context,R.mipmap.play_screen_foreground)
-            2-> getDrawable(context,R.drawable.ic_health)
-            else->getDrawable(context,R.drawable.ic_health)
+
+    private fun getDrawableFromAbstractInt(context: Context, value: Int) =
+        when (value) {
+            0 -> getDrawable(context, R.mipmap.ad_foreground)
+            1 -> getDrawable(context, R.mipmap.play_screen_foreground)
+            2 -> getDrawable(context, R.drawable.ic_health)
+            else -> getDrawable(context, R.drawable.ic_health)
         }
 
-    private suspend fun changeCounter(post:Post,type:CounterType){
-        when(type){
-            CounterType.Like-> {
-                client.post<CounterChangeDto>("https://srv-ncms.herokuapp.com/api/v1/posts/changeCounter"){
-                    CounterChangeDto(post.id,post.likeCounter,CounterType.Like)
+    private suspend fun changeCounter(post: Post, type: CounterType) {
+        when (type) {
+            CounterType.Like -> {
+                client.post<String> {
+                    url("https://srv-ncms.herokuapp.com/api/v1/posts/changeCounter")
+                    method = HttpMethod.Post
+                    body = TextContent(
+                        Gson().toJson(CounterChangeDto(post.id, post.likeCounter, type)),
+                        ContentType.Application.Json
+                    )
                 }
             }
-            CounterType.Dislike->  {
-                client.post<CounterChangeDto>("https://srv-ncms.herokuapp.com/api/v1/posts/changeCounter"){
-                    CounterChangeDto(post.id,post.dislikeCounter,CounterType.Dislike)
+            CounterType.Dislike -> {
+                client.post<String> {
+                    url("https://srv-ncms.herokuapp.com/api/v1/posts/changeCounter")
+                    method = HttpMethod.Post
+                    body = TextContent(
+                        Gson().toJson(CounterChangeDto(post.id, post.dislikeCounter, type)),
+                        ContentType.Application.Json
+                    )
                 }
             }
-            CounterType.Comment->  {
-                client.post<CounterChangeDto>("https://srv-ncms.herokuapp.com/api/v1/posts/changeCounter"){
-                    CounterChangeDto(post.id,post.commentCounter,CounterType.Comment)
+            CounterType.Comment -> {
+                client.post<String>{
+                    url("https://srv-ncms.herokuapp.com/api/v1/posts/changeCounter")
+                    method = HttpMethod.Post
+                    body=TextContent(
+                        Gson().toJson(CounterChangeDto(post.id, post.commentCounter, type)),
+                        ContentType.Application.Json
+                    )
                 }
             }
-            CounterType.Share->  {
-                client.post<CounterChangeDto>("https://srv-ncms.herokuapp.com/api/v1/posts/changeCounter"){
-                    CounterChangeDto(post.id,post.shareCounter,CounterType.Share)
+            CounterType.Share -> {
+                client.post<String>{
+                    url("https://srv-ncms.herokuapp.com/api/v1/posts/changeCounter")
+                    method = HttpMethod.Post
+                    body=TextContent(
+                        Gson().toJson(CounterChangeDto(post.id, post.shareCounter, type)),
+                        ContentType.Application.Json
+                    )
                 }
             }
         }
